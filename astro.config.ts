b015ea52f4a93d836f1885d74db900f6a083d956
Alexander Niebuhr <alexander@nbhr.io>
2025-08-13 11:03:55 +0200

    Add site to astro config

--- astro.config.ts
+++ astro.config.ts
@@ -2,5 +2,6 @@ import cloudflare from "@astrojs/cloudflare";
 import { defineConfig } from "astro/config";
 
 export default defineConfig({
+	site: 'https://ai.craftlions.com',
 	adapter: cloudflare(),
 });
