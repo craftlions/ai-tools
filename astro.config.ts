19fdcd9d841d093931242c9a9f754a8596e7d944
Alexander Niebuhr <alexander@nbhr.io>
2025-08-13 11:04:54 +0200

    Optimize build settings

--- astro.config.ts
+++ astro.config.ts
@@ -1,7 +1,34 @@
 import cloudflare from "@astrojs/cloudflare";
 import { defineConfig } from "astro/config";
+import browserslist from "browserslist";
+import { browserslistToTargets } from "lightningcss";
 
 export default defineConfig({
-	site: 'https://ai.craftlions.com',
+	site: "https://ai.craftlions.com",
 	adapter: cloudflare(),
+	vite: {
+		css: {
+			transformer: "lightningcss",
+			lightningcss: {
+				targets: browserslistToTargets(
+					browserslist([
+						"> 0.5%",
+						"last 2 versions",
+						"Firefox ESR",
+						"not dead",
+						"cover 80% in CN",
+						"unreleased versions",
+					]),
+				),
+				drafts: {
+					customMedia: true,
+				},
+			},
+		},
+		build: {
+			minify: false,
+			cssMinify: false,
+		},
+	},
+	compressHTML: false,
 });
